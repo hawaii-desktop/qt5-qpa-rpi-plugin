b57979f084670d7c23d1204a85e97fe886c83ad9
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-02-22 19:26:49 +0000

    Update Raspberry Pi hooks from dev
    This will remove unused code fixing build warnings.

--- eglfs_rpi/qeglfshooks_pi.cpp
+++ eglfs_rpi/qeglfshooks_pi.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 **
-** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
-** Contact: http://www.qt-project.org/legal
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
 **
 ** This file is part of the qmake spec of the Qt Toolkit.
 **
@@ -10,9 +10,9 @@
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia. For licensing terms and
-** conditions see http://qt.digia.com/licensing. For further information
-** use the contact form at http://qt.digia.com/contact-us.
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
@@ -23,8 +23,8 @@
 ** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
 ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
-** In addition, as a special exception, Digia gives you certain additional
-** rights. These rights are described in the Digia Qt LGPL Exception
+** As a special exception, The Qt Company gives you certain additional
+** rights. These rights are described in The Qt Company LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
 ** $QT_END_LICENSE$
@@ -77,39 +77,6 @@ static EGLNativeWindowType createDispmanxLayer(const QPoint &pos, const QSize &s
     eglWindow->height = size.height();
 
     return eglWindow;
-}
-
-// these constants are not in any headers (yet)
-#define ELEMENT_CHANGE_LAYER          (1<<0)
-#define ELEMENT_CHANGE_OPACITY        (1<<1)
-#define ELEMENT_CHANGE_DEST_RECT      (1<<2)
-#define ELEMENT_CHANGE_SRC_RECT       (1<<3)
-#define ELEMENT_CHANGE_MASK_RESOURCE  (1<<4)
-#define ELEMENT_CHANGE_TRANSFORM      (1<<5)
-
-static void moveDispmanxLayer(EGLNativeWindowType window, const QPoint &pos)
-{
-    EGL_DISPMANX_WINDOW_T *eglWindow = static_cast<EGL_DISPMANX_WINDOW_T *>(window);
-    QSize size(eglWindow->width, eglWindow->height);
-
-    VC_RECT_T dst_rect;
-    dst_rect.x = pos.x();
-    dst_rect.y = pos.y();
-    dst_rect.width = size.width();
-    dst_rect.height = size.height();
-
-    DISPMANX_UPDATE_HANDLE_T dispman_update = vc_dispmanx_update_start(0);
-    vc_dispmanx_element_change_attributes(dispman_update,
-                                          eglWindow->element,
-                                          ELEMENT_CHANGE_DEST_RECT /*change_flags*/,
-                                          0,
-                                          0,
-                                          &dst_rect,
-                                          NULL,
-                                          0,
-                                          (DISPMANX_TRANSFORM_T)0);
-
-    vc_dispmanx_update_submit_sync(dispman_update);
 }
 
 static void destroyDispmanxLayer(EGLNativeWindowType window)
